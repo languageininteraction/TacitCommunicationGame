e887d481c4934063d4861c84fc6e54bb5bf4cddc
Wessel Stoop <woseseltops@hotmail.com>
2014-11-27 09:55:02 +0100

    Created Levels #39 #41

---
+++ TCGGame/TCGGame/BoardDefinition.swift
@@ -0,0 +1,20 @@
+//
+//  BoardDefinition.swift
+//  TCGGame
+//
+//  Created by Wessel Stoop on 27/11/14.
+//  Copyright (c) 2014 gametogether. All rights reserved.
+//
+
+import Foundation
+
+class BoardDefinition: NSObject
+{
+    let width, height: Int
+
+    init(width: Int, height: Int)
+    {
+        self.width = width
+        self.height = height
+    }
+}
