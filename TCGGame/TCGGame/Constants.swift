2649e08947d04d900228bd206f85b77933a5732f
Wessel Stoop <woseseltops@hotmail.com>
2014-11-27 12:50:29 +0100

    Added the style guide colors #37

--- TCGGame/TCGGame/Constants.swift
+++ TCGGame/TCGGame/Constants.swift
@@ -31,6 +31,12 @@ let kPawnScaleOfSecondLargestWRTLargest: Float = 0.85
 let kColorLiIBlue = UIColor(red:0, green:158.0/255.0, blue:200.0/255.0, alpha:1)
 let kColorLiIBlueLighter = UIColor(red:142.0/255.0, green:207.0/255.0, blue:230.0/255.0, alpha:1)
 let kColorLiIOrange = UIColor(red:239.0/255.0, green:123.0/255.0, blue:16.0/255.0, alpha:1)
+let kColorLiIPurple = UIColor(red:108.0/255.0, green:99.0/255.0, blue:154.0/255.0, alpha:1)
+let kColorLiIBrown = UIColor(red:104.0/255.0, green:85.0/255.0, blue:70.0/255.0, alpha:1)
+let kColorLiIGreen = UIColor(red:0.0/255.0, green:164.0/255.0, blue:122.0/255.0, alpha:1)
+let kColorLiIBordeaux = UIColor(red:139.0/255.0, green:34.0/255.0, blue:31.0/255.0, alpha:1)
+let kColorLiIYellow = UIColor(red:199.0/255.0, green:178.0/255.0, blue:0.0/255.0, alpha:1)
 
 // Component colors:
 let kColorBoardFields = kColorLiIBlueLighter
+
