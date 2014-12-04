5abb1efd637c7f50f172e1f49e0cd11d4e12df11
Wessel Stoop <woseseltops@hotmail.com>
2014-12-04 17:47:05 +0100

    Added a first try add showing a TableView, probably wrong

---
+++ TCGGame/TCGGame/ChooseLevelViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseLevelViewController.swift
+//  TCGGame
+//
+//  Created by Wessel Stoop on 04-12-14
+//  Copyright (c) 2014 gametogether. All rights reserved.
+//
+
+import UIKit
+
+class ChooseLevelViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
