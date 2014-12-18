920524b31b283def3eb622d3ed423f3556113aac
Wessel Stoop <woseseltops@hotmail.com>
2014-12-18 11:41:34 +0100

    RoundActions can now figure out their own ButtonType

--- TCGGame/TCGGame/RoundAction.swift
+++ TCGGame/TCGGame/RoundAction.swift
@@ -19,11 +19,14 @@ class RoundAction: NSObject, NSCoding {
 	let type: RoundActionType
     let role: RoundRole
     let buttonIndicator: String
-    
+    let buttonType: String
+   
     init (type: RoundActionType,buttonIndicator: String,role: RoundRole) {
-		self.type = type
+        self.type = type
         self.role = role
         self.buttonIndicator = buttonIndicator
+        self.buttonType = induceButtonType(buttonIndicator) as String
+        
 	}
 	
     func encodeWithCoder(coder: NSCoder) {
@@ -31,13 +34,25 @@ class RoundAction: NSObject, NSCoding {
         coder.encodeInt(Int32(role.rawValue),forKey:"role")
         coder.encodeObject(self.buttonIndicator,forKey:"buttonIndicator")
     }
-	
 
     required init (coder decoder: NSCoder)
     {
         self.type = RoundActionType.Tap
         self.role = RoundRole(rawValue: Int(decoder.decodeIntForKey("role")))!
         self.buttonIndicator = decoder.decodeObjectForKey("buttonIndicator") as String
+        self.buttonType = induceButtonType(buttonIndicator) as String
     }
     
+}
+
+func induceButtonType(buttonIndicator: String) -> String
+{
+    if (find(["north","east","south","west"],buttonIndicator) != nil)
+    {
+        return "move"
+    }
+    else
+    {
+        return "rotate"
+    }
 }
