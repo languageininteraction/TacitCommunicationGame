335f84f33382fd78d8927fd773377f59dd374a39
Wessel Stoop <woseseltops@hotmail.com>
2015-01-16 15:34:53 +0100

    Levels can now pack and unpack themselves

--- TCGGame/TCGGame/Level.swift
+++ TCGGame/TCGGame/Level.swift
@@ -28,6 +28,8 @@ class Level: NSObject
     var startItemsPlayer1: [ItemDefinition]
     var startItemsPlayer2: [ItemDefinition]
     
+    // MARK: - Starting the level from a JSON file
+    
     init(filename:String)
     {
         // Read in the level:
@@ -64,7 +66,10 @@ class Level: NSObject
 		}
     }
 	
-    //Loads a very basic level, for 'manual' customization
+    // MARK: - Starting a simple basic level
+    
+    //(For manual customization by the level generator)
+    
     init(name : String)
     {
         self.name = name
@@ -87,7 +92,114 @@ class Level: NSObject
         
     }
     
-	func itemOfTypeIsAvailable(itemType: ItemType) -> Bool {
-		return itemType == ItemType.Move ? moveItemAvailable : itemType == ItemType.See ? seeItemAvailable : giveItemAvailable
-	}
+    // MARK: - Encoding and decoding
+    
+    func encodeWithCoder(coder: NSCoder)
+    {
+
+        // MARK: 1. Collect the things we want to encode
+        
+        //Basic info
+        var objectsToEncode : Array<(String,AnyObject)>= [("name",self.name), ("boardWidth",self.board.width),("boardHeight",self.board.height),
+            ("pawnPlayer1Shape",self.pawnPlayer1.shape.rawValue),("pawnPlayer2Shape",self.pawnPlayer2.shape.rawValue),
+        ]
+ 
+        //The configurations
+        let configsToEncode : Array<(String,PawnConfiguration)> = [("startConfig1",self.startConfigurationPawn1),("startConfig2",self.startConfigurationPawn2),("goalConfig1",self.goalConfigurationPawn1),("goalConfig2",self.goalConfigurationPawn2)]
+        
+        var name : String? = nil
+        var config : PawnConfiguration? = nil
+        
+        for item in configsToEncode
+        {
+            name = item.0
+            config = item.1
+            objectsToEncode = objectsToEncode + config!.getObjectsToEncode(configName: name!);
+        }
+        
+        //The items
+        
+        
+        //var listToEncode: [Dictionary<String,AnyObject>] = [["test":0],["test1":1,"test2":2]]
+        var listToEncode: [Dictionary<String,AnyObject>] = []
+
+        for (title,itemlist) in [("startItemsPlayer1",self.startItemsPlayer1),("startItemsPlayer2",self.startItemsPlayer2)]
+        {
+            listToEncode = []
+            
+            for item in itemlist
+            {
+                listToEncode.append(item.asDict())
+            }
+
+            coder.encodeObject(listToEncode,forKey: title);
+            
+        }
+        
+        //Some bools
+        let boolsToEncode : Array<(String,Bool)> = [("moveItemAvailable",self.moveItemAvailable),("seeItemAvailable",self.seeItemAvailable),("giveItemAvailable",self.giveItemAvailable)]
+        
+        // MARK: 2. Actual encoding
+
+        var key : String? = nil
+        var value : AnyObject? = nil
+        
+        for item in objectsToEncode
+        {
+            key = item.0
+            value = item.1
+            
+            coder.encodeObject(value!, forKey: key!)
+        }
+        
+        for (key, boolValue) in boolsToEncode
+        {
+            coder.encodeBool(boolValue, forKey: key)
+        }
+        
+    }
+
+    func itemOfTypeIsAvailable(itemType: ItemType) -> Bool {
+        return itemType == ItemType.Move ? moveItemAvailable : itemType == ItemType.See ? seeItemAvailable : giveItemAvailable
+    }
+
+    required init (coder decoder: NSCoder)
+    {
+        self.name = decoder.decodeObjectForKey("name") as String
+        self.board = BoardDefinition(width: decoder.decodeObjectForKey("boardWidth") as Int, height: decoder.decodeObjectForKey("boardHeight") as Int)
+        self.pawnPlayer1 = PawnDefinition(shape: PawnShape(rawValue: decoder.decodeObjectForKey("pawnPlayer1Shape") as Int)!, color: kColorLiIOrange)
+        self.pawnPlayer2 = PawnDefinition(shape: PawnShape(rawValue: decoder.decodeObjectForKey("pawnPlayer2Shape") as Int)!, color: kColorLiIYellow)
+        
+        self.startConfigurationPawn1 = PawnConfiguration(x: decoder.decodeObjectForKey("startConfig1x") as Int, y: decoder.decodeObjectForKey("startConfig1y") as Int, rotation: Direction(rawValue: decoder.decodeObjectForKey("startConfig1Rotation") as Int)!)
+        self.startConfigurationPawn2 = PawnConfiguration(x: decoder.decodeObjectForKey("startConfig2x") as Int, y: decoder.decodeObjectForKey("startConfig2y") as Int, rotation: Direction(rawValue: decoder.decodeObjectForKey("startConfig2Rotation") as Int)!)
+        
+        self.goalConfigurationPawn1 = PawnConfiguration(x: decoder.decodeObjectForKey("goalConfig1x") as Int, y: decoder.decodeObjectForKey("goalConfig1y") as Int, rotation: Direction(rawValue: decoder.decodeObjectForKey("goalConfig1Rotation") as Int)!)
+        
+        self.goalConfigurationPawn2 = PawnConfiguration(x: decoder.decodeObjectForKey("goalConfig2x") as Int, y: decoder.decodeObjectForKey("goalConfig2y") as Int, rotation: Direction(rawValue: decoder.decodeObjectForKey("goalConfig2Rotation") as Int)!)
+        
+        self.moveItemAvailable = decoder.decodeBoolForKey("moveItemAvailable")
+        self.seeItemAvailable = decoder.decodeBoolForKey("seeItemAvailable")
+        self.giveItemAvailable = decoder.decodeBoolForKey("giveItemAvailable")
+        
+        var itemTypeRawValue : Int = 0
+        
+        self.startItemsPlayer1 = []
+        var encodedStartItemsPlayer1 : Array = decoder.decodeObjectForKey("startItemsPlayer1") as NSArray
+        
+        for itemDict in encodedStartItemsPlayer1
+        {
+            itemTypeRawValue = itemDict["itemType"] as Int
+            self.startItemsPlayer1.append(ItemDefinition(itemType: ItemType(rawValue: itemTypeRawValue)!, endlessUse: itemDict["endlessUse"] as Bool, nrUses: (itemDict["nrUses"] as Int)))
+        }
+        
+        self.startItemsPlayer2 = []
+        var encodedStartItemsPlayer2 : Array = decoder.decodeObjectForKey("startItemsPlayer2") as NSArray
+        
+        for itemDict in encodedStartItemsPlayer2
+        {
+            itemTypeRawValue = itemDict["itemType"] as Int
+            self.startItemsPlayer2.append(ItemDefinition(itemType: ItemType(rawValue: itemTypeRawValue)!, endlessUse: itemDict["endlessUse"] as Bool, nrUses: (itemDict["nrUses"] as Int)))
+        }
+        
+    }
 }
