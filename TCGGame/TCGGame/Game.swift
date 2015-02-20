eb0196ab507f7ddc9fd083f2ef316802a2c2ec8c
Jop van Heesch <gametogethersoftware@gmail.com>
2015-02-20 13:53:56 +0100

    Cleanup

--- TCGGame/TCGGame/Game.swift
+++ TCGGame/TCGGame/Game.swift
@@ -41,8 +41,6 @@ class Game: NSObject
         
     override init()
     {
-		println("Game init")
-		
 		self.nBeginnerLevels = beginnerLevelNames.count
 		
         self.currentDifficulty = Difficulty.Beginner
@@ -53,12 +51,6 @@ class Game: NSObject
         self.nCompletedLevels[Difficulty.Expert] = 0
     }
 	
-	// temp! to test why too many Games are initialized
-	convenience init(test: Int) {
-		self.init()
-		println("test = \(test)")
-	}
-
     func goToHighestBeginnerLevel() //Not finished yet, this will be much more smart/complicated
     {
         self.indexCurrentLevel++
